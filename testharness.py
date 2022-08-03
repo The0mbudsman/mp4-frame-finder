05efdd577084127db799726785c827043cfef64e
The0mbudsman <37667514+The0mbudsman@users.noreply.github.com>
2022-08-03 21:06:26 +0100

    Update testharness.py

--- testharness.py
+++ testharness.py
@@ -1,39 +1,47 @@
-from frameFinder import frameFinder
-import os
 from glob import glob
-
+from frameFinder import frameFinder
 import ffmpeg
+from colorthief import ColorThief
+import os
 
-basedir = "testrun_no_cleanup"
-filepath = "../media/MP4-FROM-WEB-total/*.mp4"
-
+basedir = "5k_new_cleanup_N_SR_TB"
+filepath = "../media/MP4-FROM-WEB-h264/*.mp4"
 correct = []
-toomany = []
 toofew = []
+toomany = []
 number_of_files = len(sorted(glob(filepath)))
 for file in sorted(glob(filepath)):
-    print(f"======={file}======")
     tokens = file.split("/")
     file_name_only = tokens[len(tokens)-1].split(".")[0]
-    
-    # Get frameFinder to do the file
-    ff = frameFinder(clustersize=4096, output_dir_name=f"{basedir}/{file_name_only}", nocleanup=True)
+    ff = frameFinder(clustersize=4096, output_dir_name=f"{basedir}/{file_name_only}")
     ff.process_image(file)
-    
-    # Get ffmpeg to do the file
     try:
         process = (ffmpeg
-            .input(file)
-            .output(f"{basedir}/{file_name_only}/header_0/yi%03d.jpg", vf="select='eq(pict_type,PICT_TYPE_I)'", vsync="passthrough", loglevel="quiet")
+            .input(file, skip_frame="nokey")
+            .output(f"{basedir}/{file_name_only}/header_0/yi%03d.jpg", vsync="0", frame_pts="true", loglevel="quiet")
             .run()
             )
+        files= glob(f"{basedir}/{file_name_only}/header_0/*.jpg")
+        for img in files:
+            color_thief = ColorThief(img)
+            try:
+                palette = color_thief.get_palette(color_count=10)[1:]
+                delete = all(colour == palette[0] for colour in palette)
+                if delete:
+                    os.remove(img)
+            except:
+                continue
+
         ffmpeg_files= glob(f"{basedir}/{file_name_only}/header_0/yi*.jpg")
     except:
         ffmpeg_files= [0]
 
+    process = ffmpeg.input(file).output(f"{basedir}/{file_name_only}/header_0/{file_name_only}.h264", vcodec="copy", frame_pts="true", an=None, **{'bsf:v': 'h264_mp4toannexb'}).run()
+
+
+
     framefinder_files= glob(f"{basedir}/{file_name_only}/header_0/frame*jpg")
 
-     # Compare nunmbers
     if (len(ffmpeg_files) == len(framefinder_files)):
         correct.append(f"{basedir}/{file_name_only}")
     elif (len(ffmpeg_files) > len(framefinder_files)):
